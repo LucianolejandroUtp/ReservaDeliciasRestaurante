0033b55181f8d1d22749dbb4b454fe0faa8bd3a8
lucianolejandroUTP <1633775@utp.edu.pe>
2024-06-26 16:23:33 -0500

    feat(Java + templates): :sparkles: Eliminar reserva funcionando

--- src/main/java/com/delicias/reserva/controllers/ReservaController.java
+++ src/main/java/com/delicias/reserva/controllers/ReservaController.java
@@ -7,12 +7,16 @@ import com.delicias.reserva.servicios.MesaService;
 import com.delicias.reserva.servicios.ReservaService;
 import com.delicias.reserva.servicios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -32,7 +36,7 @@ public class ReservaController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping(path = {"/reservas", "/lista"})
+    @GetMapping(path = { "/reservas", "/lista" })
     public String getAllReservas(Model model) {
         List<Reservas> reservas = reservaService.getAllReservas();
         List<Mesas> mesas = mesaService.getAllMesas();
@@ -44,23 +48,20 @@ public class ReservaController {
         return "reserva";
     }
 
-
     @PostMapping("/create")
-    public String create(
-            @RequestParam(name ="dato1", required = false) String dato1,
-            @RequestParam(name ="dato2", required = false) String dato2,
-            @RequestParam(name ="dato3", required = false) String dato3,
-//            @RequestParam(name ="dato4", required = false) String dato4,
-//            @RequestParam(name ="dato5", required = false) String dato5,
-            @RequestParam(name ="dato6", required = false) String dato6,
-            @RequestParam(name ="dato7", required = false) String dato7
-            ) {
+    public String create(@RequestParam(name = "dato1", required = false) String dato1,
+            @RequestParam(name = "dato2", required = false) String dato2,
+            @RequestParam(name = "dato3", required = false) String dato3,
+            // @RequestParam(name ="dato4", required = false) String dato4,
+            // @RequestParam(name ="dato5", required = false) String dato5,
+            @RequestParam(name = "dato6", required = false) String dato6,
+            @RequestParam(name = "dato7", required = false) String dato7) {
 
         System.out.println("---" + dato1 + "---");
         System.out.println("---" + dato2 + "---");
         System.out.println("---" + dato3 + "---");
-//        System.out.println("---" + dato4 + "---");
-//        System.out.println("---" + dato5 + "---");
+        // System.out.println("---" + dato4 + "---");
+        // System.out.println("---" + dato5 + "---");
         System.out.println("---" + dato6 + "---");
         DateTimeFormatter formatoEntradaFecha = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         DateTimeFormatter formatoEntradaHora = DateTimeFormatter.ofPattern("hh:mm a");
@@ -72,12 +73,10 @@ public class ReservaController {
         System.out.println("Fecha formateada: " + fecha);
         System.out.println("Hora formateada: " + hora);
 
-
         Usuarios miUsuario = usuarioService.getUsuarioById(Long.parseLong(dato6));
         Mesas miMesa = mesaService.getMesaById(Long.parseLong(dato7));
 
         Reservas miReserva = new Reservas();
-
 
         miReserva.setFecha(fecha);
         miReserva.setHora(hora);
@@ -90,5 +89,11 @@ public class ReservaController {
         return "redirect:/distrito/lista";
     }
 
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        reservaService.deleteReserva(id);
+        return ResponseEntity.ok("Eliminación exitosa");
+    }
 
 }
