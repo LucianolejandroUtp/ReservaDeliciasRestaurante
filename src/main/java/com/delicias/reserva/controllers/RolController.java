f14b83ca37e03713238e1a0753f3b2d41356eb35
lucianolejandroUTP <1633775@utp.edu.pe>
2024-07-04 14:32:40 -0500

    feat: Add delete functionality to RolController
    The `RolController` now includes a `delete` method that allows for deleting a role by its ID. This functionality has been added to enhance the CRUD operations for roles.

--- src/main/java/com/delicias/reserva/controllers/RolController.java
+++ src/main/java/com/delicias/reserva/controllers/RolController.java
@@ -3,12 +3,16 @@ package com.delicias.reserva.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.delicias.reserva.modelos.Roles;
 import com.delicias.reserva.servicios.RolService;
@@ -29,10 +33,19 @@ public class RolController {
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam(name ="createRol", required = false, defaultValue = "Mundo") String createRol, Model model) {
+    public String create(@RequestParam(name ="createDato01", required = false, defaultValue = "Mundo") String createRol, Model model) {
         Roles miRol = new Roles();
         miRol.setDescripcion(createRol);
         rolService.saveRol(miRol);
         return "redirect:/rol/lista";
+    }
+
+
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        rolService.deleteRol(id);
+        return ResponseEntity.ok("Eliminación exitosa");
     }
 }
