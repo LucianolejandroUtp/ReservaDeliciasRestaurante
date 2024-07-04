1e8c5a5c962f279029db5fecd31c31a626da3545
lucianolejandroUTP <1633775@utp.edu.pe>
2024-07-04 12:44:34 -0500

    chore: Rename MesasRepository to MesaRepository

--- src/main/java/com/delicias/reserva/repositorios/MesaRepository.java
+++ src/main/java/com/delicias/reserva/repositorios/MesaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MesasRepository extends JpaRepository<Mesas, Long> {
+public interface MesaRepository extends JpaRepository<Mesas, Long> {
 }
