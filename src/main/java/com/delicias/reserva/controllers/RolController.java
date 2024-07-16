a15060efbaf9a262649e55b2e743ccc395759732
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-16 12:05:06 -0500

    feat: Formateo de código en RolController

--- src/main/java/com/delicias/reserva/controllers/RolController.java
+++ src/main/java/com/delicias/reserva/controllers/RolController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.delicias.reserva.modelos.Roles;
 import com.delicias.reserva.servicios.RolService;
 
-
 @Controller
 @RequestMapping("/rol")
 public class RolController {
@@ -26,7 +25,7 @@ public class RolController {
     @Autowired
     private RolService rolService;
 
-    @GetMapping(path = {"/roles", "/lista"})
+    @GetMapping(path = { "/roles", "/lista" })
     public String getAllRoles(Model model) {
         List<Roles> roles = rolService.getAllRoles();
         model.addAttribute("roles", roles);
@@ -34,38 +33,40 @@ public class RolController {
     }
 
     @PostMapping("/create")
-    public String create(@RequestParam(name ="createDato01", required = false, defaultValue = "Mundo") String createRol, Model model) {
+    public String create(
+            @RequestParam(name = "createDato01", required = false, defaultValue = "Mundo") String createRol,
+            Model model) {
         Roles miRol = new Roles();
         miRol.setDescripcion(createRol);
         rolService.saveRol(miRol);
         return "redirect:/rol/lista";
     }
 
-@GetMapping("/{id}")
-@ResponseBody
-public ResponseEntity<Roles> obtenerRol(@PathVariable Long id) {
-    Roles rol = rolService.getRolById(id);
-    return ResponseEntity.ok(rol);
-}
-
-@PostMapping("/update/{id}")
-@ResponseBody
-public ResponseEntity<?> update(@PathVariable(name = "id") Long id, @RequestBody Roles miObjeto) {
-    Roles rolDB = rolService.getRolById(id);
-
-    System.out.println("Objeto: " + miObjeto.toString());
-    System.out.println("Entrando al update de reserva");
-
-    if (rolDB.getDescripcion() == null || rolDB.getDescripcion() != miObjeto.getDescripcion()) {
-        rolDB.setDescripcion(miObjeto.getDescripcion());
-    }
-    if(rolDB.getEstado() == null || rolDB.getEstado() != miObjeto.getEstado()) {
-        rolDB.setEstado(miObjeto.getEstado());
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Roles> obtenerRol(@PathVariable Long id) {
+        Roles rol = rolService.getRolById(id);
+        return ResponseEntity.ok(rol);
     }
 
-    rolService.saveRol(rolDB);
-    return ResponseEntity.ok("Actualización exitosa");
-}
+    @PostMapping("/update/{id}")
+    @ResponseBody
+    public ResponseEntity<?> update(@PathVariable(name = "id") Long id, @RequestBody Roles miObjeto) {
+        Roles rolDB = rolService.getRolById(id);
+
+        System.out.println("Objeto: " + miObjeto.toString());
+        System.out.println("Entrando al update de reserva");
+
+        if (rolDB.getDescripcion() == null || rolDB.getDescripcion() != miObjeto.getDescripcion()) {
+            rolDB.setDescripcion(miObjeto.getDescripcion());
+        }
+        if (rolDB.getEstado() == null || rolDB.getEstado() != miObjeto.getEstado()) {
+            rolDB.setEstado(miObjeto.getEstado());
+        }
+
+        rolService.saveRol(rolDB);
+        return ResponseEntity.ok("Actualización exitosa");
+    }
 
     @DeleteMapping("/delete/{id}")
     @ResponseBody
