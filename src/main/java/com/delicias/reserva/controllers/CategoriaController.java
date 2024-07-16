06b24dc4186dc313e078f564c1728678118d91f8
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-16 12:03:38 -0500

    feat(controller): Agregar funcionalidad de creación y eliminación de categorías
    Se agregó el método "create" en el controlador `CategoriaController` para manejar la creación de nuevas categorías. También se implementó el método "delete" para eliminar una categoría por su ID. Estos cambios mejoran las operaciones CRUD para las categorías.

--- src/main/java/com/delicias/reserva/controllers/CategoriaController.java
+++ src/main/java/com/delicias/reserva/controllers/CategoriaController.java
@@ -3,10 +3,17 @@ package com.delicias.reserva.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.delicias.reserva.modelos.Categorias;
 import com.delicias.reserva.servicios.CategoriaService;
@@ -17,10 +24,25 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
 
-    @GetMapping(path = {"/categorias", "/lista"})
+    @GetMapping(path = { "/categorias", "/lista" })
     public String getAllCategorias(Model model) {
         List<Categorias> categorias = categoriaService.getAllCategorias();
         model.addAttribute("categorias", categorias);
         return "categoria";
+    }
+
+    @PostMapping("/create")
+    public String create(@RequestParam(name = "createDato01", required = false, defaultValue = "Mundo") String createCategoria, Model model) {
+        Categorias miCategoria = new Categorias();
+        miCategoria.setDescripcion(createCategoria);
+        categoriaService.saveCategoria(miCategoria);
+        return "redirect:/categoria/lista";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        categoriaService.deleteCategoria(id);
+        return ResponseEntity.ok("Eliminación exitosa");
     }
 }
