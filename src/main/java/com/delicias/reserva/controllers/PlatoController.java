35bcd5e54a38acb2da029a687ba3af525eae37bf
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-17 20:30:23 -0500

    feat: Agregar método de eliminación de plato en el controlador PlatoController
    Se agregó el método `delete` en el controlador `PlatoController` para permitir la eliminación de un plato específico. Esto mejora la funcionalidad del sistema al permitir a los usuarios eliminar platos de la lista de platos disponibles.

--- src/main/java/com/delicias/reserva/controllers/PlatoController.java
+++ src/main/java/com/delicias/reserva/controllers/PlatoController.java
@@ -4,12 +4,16 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.delicias.reserva.modelos.Categorias;
 import com.delicias.reserva.modelos.Platos;
@@ -58,5 +62,13 @@ public class PlatoController {
         miPlato.setCategorias(miCategoria);
         platoService.savePlato(miPlato);
         return "redirect:/plato/lista";
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        platoService.deletePlato(id);
+        return ResponseEntity.ok("Eliminación exitosa");
     }
 }
