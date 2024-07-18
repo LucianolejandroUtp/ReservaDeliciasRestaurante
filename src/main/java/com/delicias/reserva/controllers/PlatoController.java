28c471b4ce6c3370f81695bc02bc24af3b9ee127
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-17 20:22:33 -0500

    feat: Agregar método para crear un nuevo plato
    Se agregó el método `create` en el controlador `PlatoController` para crear un nuevo plato con los datos proporcionados. Esto permite a los usuarios agregar platos al sistema y mejorar la funcionalidad de gestión de platos.

--- src/main/java/com/delicias/reserva/controllers/PlatoController.java
+++ src/main/java/com/delicias/reserva/controllers/PlatoController.java
@@ -1,12 +1,15 @@
 package com.delicias.reserva.controllers;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.delicias.reserva.modelos.Categorias;
 import com.delicias.reserva.modelos.Platos;
@@ -23,11 +26,37 @@ public class PlatoController {
     private CategoriaService categoriaService;
 
     @GetMapping(path = { "/platos", "/lista" })
-    public String getAllPlatos(Model model){
+    public String getAllPlatos(Model model) {
         List<Platos> platos = platoService.getAllPlatos();
         List<Categorias> categorias = categoriaService.getAllCategorias();
         model.addAttribute("platos", platos);
         model.addAttribute("categorias", categorias);
         return "plato";
+    }
+
+    @PostMapping("/create")
+    public String create(
+            @RequestParam(name = "createDato01", required = false) String dato01,
+            @RequestParam(name = "createDato02", required = false) String dato02,
+            @RequestParam(name = "createDato03", required = false) BigDecimal dato03,
+            @RequestParam(name = "createDato04", required = false) Integer dato04,
+            @RequestParam(name = "createDato05", required = false) Integer dato05,
+            @RequestParam(name = "createDato06", required = false) Boolean dato06,
+            @RequestParam(name = "createDato07", required = false) String dato07,
+            @RequestParam(name = "createDato08", required = false) String dato08,
+            @RequestParam(name = "createDato09", required = false) String dato09,
+            @RequestParam(name = "createDato10", required = false) Long dato10) {
+
+        Categorias miCategoria = categoriaService.getCategoriaById(dato10);
+
+        Platos miPlato = new Platos();
+        miPlato.setNombre(dato01);
+        miPlato.setDescripcion(dato02);
+        miPlato.setPrecio(dato03);
+        miPlato.setDisponible(dato06);
+        miPlato.setStock(dato04);
+        miPlato.setCategorias(miCategoria);
+        platoService.savePlato(miPlato);
+        return "redirect:/plato/lista";
     }
 }
