1887fb3368b035e7efb49ccf93ef403ce4b95095
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-18 16:23:43 -0500

    feat: Actualizar método de actualización de bebida en BebidaController
    Se actualizó el método `update` en el controlador `BebidaController` para permitir la actualización de una bebida con los datos proporcionados. Esto asegura que los cambios realizados en la vista "bebida.html" se reflejen correctamente en el sistema.

--- src/main/java/com/delicias/reserva/controllers/BebidaController.java
+++ src/main/java/com/delicias/reserva/controllers/BebidaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -71,6 +72,34 @@ public class BebidaController {
         return ResponseEntity.ok(bebida);
     }
 
+    @PostMapping("/update/{id}")
+    @ResponseBody
+    public ResponseEntity<?> update (@PathVariable(name= "id") Long id, @RequestBody Bebidas miObjeto){
+        Bebidas bebidaDB = bebidaService.getBebidaById(id);
+        Categorias categoriaDB = categoriaService.getCategoriaById(miObjeto.getCategorias().getId());
+
+        if (bebidaDB.getNombre() == null || bebidaDB.getNombre().isEmpty() || bebidaDB.getNombre() != miObjeto.getNombre()){
+            bebidaDB.setNombre(miObjeto.getNombre());
+        }
+        if (bebidaDB.getDescripcion() == null || bebidaDB.getDescripcion().isEmpty() || bebidaDB.getDescripcion() != miObjeto.getDescripcion()){
+            bebidaDB.setDescripcion(miObjeto.getDescripcion());
+        }
+        if (bebidaDB.getPrecio() == null || bebidaDB.getPrecio().equals(0) || bebidaDB.getPrecio() != miObjeto.getPrecio()){
+            bebidaDB.setPrecio(miObjeto.getPrecio());
+        }
+        if (bebidaDB.getStock() == null || bebidaDB.getStock().equals(0) || bebidaDB.getStock() != miObjeto.getStock()){
+            bebidaDB.setStock(miObjeto.getStock());
+        }
+        if (bebidaDB.getEstado() == null || bebidaDB.getEstado().isEmpty() || bebidaDB.getEstado() != miObjeto.getEstado()){
+            bebidaDB.setEstado(miObjeto.getEstado());
+        }
+        if (!bebidaDB.getCategorias().equals(categoriaDB)){
+            bebidaDB.setCategorias(categoriaDB);
+        }
+        bebidaService.saveBebida(bebidaDB);
+
+        return ResponseEntity.ok("Actualización exitosa");
+    }
 
     @DeleteMapping("/delete/{id}")
     @ResponseBody
