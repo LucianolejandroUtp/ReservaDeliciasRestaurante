1be08c8e03a9b3fe48845a7c294b50cdacf25513
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-18 00:17:10 -0500

    feat: Actualizar tabla de platos en el campo "disponible"
    Se actualizó la tabla de platos en el archivo "create_platos_table.sql" para modificar el campo "disponible" de bit(1) a enum.

--- src/main/resources/db/changelog/create_platos_table.sql
+++ src/main/resources/db/changelog/create_platos_table.sql
@@ -1,17 +1,16 @@
 -- liquibase formatted sql
 -- changeset Lucy:createPlatosTable
-
-CREATE TABLE platos
-(
-    id            BIGINT                                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre        VARCHAR(100)                             NOT NULL,
-    descripcion   TEXT                                     NOT NULL,
-    precio        DECIMAL(10, 2)                           NOT NULL,
-    disponible    BIT                                      NOT NULL DEFAULT 1,
-    stock         INT                                      NOT NULL DEFAULT 0,
-    estado        enum ('ACTIVO', 'INACTIVO', 'ELIMINADO') NULL     DEFAULT 'ACTIVO',
-    created_at    TIMESTAMP                                NULL     DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP                                NULL ON UPDATE CURRENT_TIMESTAMP,
-    categorias_id BIGINT                                   NOT NULL,
-    FOREIGN KEY (categorias_id) REFERENCES categorias (id)
-);
+CREATE TABLE
+    platos (
+        id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        descripcion TEXT NOT NULL,
+        precio DECIMAL(10, 2) NOT NULL,
+        disponible enum ('DISPONIBLE', 'NO_DISPONIBLE') NOT NULL DEFAULT 'DISPONIBLE',
+        stock INT NOT NULL DEFAULT 0,
+        estado enum ('ACTIVO', 'INACTIVO', 'ELIMINADO') NULL DEFAULT 'ACTIVO',
+        created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+        categorias_id BIGINT NOT NULL,
+        FOREIGN KEY (categorias_id) REFERENCES categorias (id)
+    );
