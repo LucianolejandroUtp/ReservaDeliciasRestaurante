954ba462c1e243d69b50283865c9ea6ac5b18100
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-19 12:39:26 -0500

    feat: Agregar funcionalidad de crear y eliminar pedidos

--- src/main/java/com/delicias/reserva/controllers/PedidosController.java
+++ src/main/java/com/delicias/reserva/controllers/PedidosController.java
@@ -3,17 +3,25 @@ package com.delicias.reserva.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.delicias.reserva.modelos.Distritos;
+import com.delicias.reserva.modelos.Bebidas;
 import com.delicias.reserva.modelos.Pedidos;
-import com.delicias.reserva.modelos.Roles;
-import com.delicias.reserva.servicios.DistritoService;
+import com.delicias.reserva.modelos.Platos;
+import com.delicias.reserva.modelos.Reservas;
+import com.delicias.reserva.servicios.BebidaService;
 import com.delicias.reserva.servicios.PedidoService;
-import com.delicias.reserva.servicios.RolService;
+import com.delicias.reserva.servicios.PlatoService;
+import com.delicias.reserva.servicios.ReservaService;
 
 @Controller
 @RequestMapping("/pedido")
@@ -21,19 +29,49 @@ public class PedidosController {
     @Autowired
     private PedidoService pedidoService;
     @Autowired
-    private DistritoService distritoService;
+    private ReservaService reservaService;
     @Autowired
-    private RolService rolService;
+    private PlatoService platoService;
+    @Autowired
+    private BebidaService bebidaService;
 
     @GetMapping(path = { "/pedidos", "/lista" })
     public String getAllPedidos(Model model) {
         List<Pedidos> pedidos = pedidoService.getAllPedidos();
-        List<Distritos> distritos = distritoService.getAllDistritos();
-        List<Roles> roles = rolService.getAllRoles();
+        List<Reservas> reservas = reservaService.getAllReservas();
+        List<Platos> platos = platoService.getAllPlatos();
+        List<Bebidas> bebidas = bebidaService.getAllBebidas();
         model.addAttribute("pedidos", pedidos);
-        model.addAttribute("distritos", distritos);
-        model.addAttribute("roles", roles);
+        model.addAttribute("reservas", reservas);
+        model.addAttribute("platos", platos);
+        model.addAttribute("bebidas", bebidas);
         return "pedido";
     }
 
+    @PostMapping("/create")
+    public String create(
+        @RequestParam(name = "createDato01", required = false) String dato01,
+            @RequestParam(name = "createDato20", required = false) Long dato20,
+            @RequestParam(name = "createDato21", required = false) Long dato21,
+            @RequestParam(name = "createDato22", required = false) Long dato22,
+            @RequestParam(name = "createDato23", required = false) Long dato23){
+        Reservas miReserva = reservaService.getReservaById(dato20);
+        Platos miPlato = platoService.getPlatoById(dato21);
+        Bebidas miBebida = bebidaService.getBebidaById(dato22);
+
+        Pedidos miPedido = new Pedidos();
+        miPedido.setReservas(miReserva);
+        miPedido.setPlatos(miPlato);
+        miPedido.setBebidas(miBebida);
+
+        pedidoService.savePedido(miPedido);
+        return "redirect:/pedido/lista";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        pedidoService.deletePedido(id);
+        return ResponseEntity.ok("Eliminación exitosa");
+    }
 }
