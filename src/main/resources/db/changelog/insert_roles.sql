8a276bd4abb66c4504f1815f533996c859df8341
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-19 19:17:30 -0500

    feat: Agregar roles de usuario ADMIN, RECEPCIONISTA, USER e INVITADO

--- src/main/resources/db/changelog/insert_roles.sql
+++ src/main/resources/db/changelog/insert_roles.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 -- changeset Lucy:insertRolesTable
 
-INSERT INTO delicias_rest.roles (descripcion) VALUES ('ADMIN');
+INSERT INTO delicias_rest.roles (descripcion) VALUES ('ADMIN,RECEPCIONISTA,USER,INVITADO');
 INSERT INTO delicias_rest.roles (descripcion) VALUES ('RECEPCIONISTA');
 INSERT INTO delicias_rest.roles (descripcion) VALUES ('USER');
 INSERT INTO delicias_rest.roles (descripcion) VALUES ('INVITADO');
