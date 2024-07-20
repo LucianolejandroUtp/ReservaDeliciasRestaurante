08736d0e6f8889fb862f594990ed67de53cf446d
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-20 08:28:10 -0500

    feat: Agregar servicio de distritos al controlador de autenticación

--- src/main/java/com/delicias/reserva/controllers/AuthController.java
+++ src/main/java/com/delicias/reserva/controllers/AuthController.java
@@ -1,12 +1,21 @@
 package com.delicias.reserva.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.delicias.reserva.modelos.Distritos;
+import com.delicias.reserva.servicios.DistritoService;
 
 @Controller
 @RequestMapping(path = {"/auth"})
 public class AuthController {
+    @Autowired
+    private DistritoService distritoService;
 
     @GetMapping(path = {"/login"})
     public String login() {
@@ -14,7 +23,9 @@ public class AuthController {
     }
 
     @GetMapping(path = {"/register"})
-    public String register() {
+    public String register(Model model) {
+        List<Distritos> distritos = distritoService.getAllDistritos();
+        model.addAttribute("distritos", distritos);
         return "auth/register";
     }
 
