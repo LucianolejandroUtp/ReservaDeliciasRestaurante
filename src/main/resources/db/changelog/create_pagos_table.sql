956a07f716eadb987bb5d0770ca4c0d20c0b9a9e
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-21 16:37:53 -0500

    feat: Add pagos table to the database schema
    A new table called "pagos" was added to the database schema. This table includes columns for "id", "monto", "fecha", "hora", "estado", "created_at", "updated_at", and "pedidos_id". The "pedidos_id" column is a foreign key referencing the "pedidos" table. The purpose of this change is to support payment functionality in the application.

---
+++ src/main/resources/db/changelog/create_pagos_table.sql
@@ -0,0 +1,14 @@
+-- liquibase formatted sql
+-- changeset Lucy:createPagosTable
+
+CREATE TABLE pagos (
+    id            BIGINT                                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    monto         DECIMAL(10, 2)                           NOT NULL,
+    fecha         DATE                                     NOT NULL,
+    hora          TIME                                     NULL,
+    estado        enum ('PENDIENTE', 'APROBADO', 'RECHAZADO') NULL     DEFAULT 'PENDIENTE',
+    created_at    TIMESTAMP                                NULL     DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP                                NULL ON UPDATE CURRENT_TIMESTAMP,
+    pedidos_id    BIGINT                                   NOT NULL,
+    FOREIGN KEY (pedidos_id) REFERENCES pedidos (id)
+);
