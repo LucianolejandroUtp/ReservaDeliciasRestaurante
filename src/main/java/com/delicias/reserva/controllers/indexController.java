99dfa0c5ce3eebc0de7f574111adc94ed5d255ee
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-20 19:28:50 -0500

    feat: Agregar lista de distritos en la página de inicio
    Se agregó una lista de distritos en la página de inicio para mostrar opciones al usuario al seleccionar un distrito. Esto se logró modificando el controlador `indexController.java` para obtener todos los distritos y pasándolos al modelo. Además, se renombró el archivo `indexJolaus.html` a `index.html` en la carpeta de plantillas.

--- src/main/java/com/delicias/reserva/controllers/indexController.java
+++ src/main/java/com/delicias/reserva/controllers/indexController.java
@@ -1,15 +1,27 @@
 package com.delicias.reserva.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.delicias.reserva.modelos.Distritos;
+import com.delicias.reserva.servicios.DistritoService;
 
 @Controller
 @RequestMapping
 public class indexController {
+    @Autowired
+    private DistritoService distritoService;
 
     @GetMapping(path = {"/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        List<Distritos> distritos = distritoService.getAllDistritos();
+        model.addAttribute("distritos", distritos);
+
         return "index";
     }
 
