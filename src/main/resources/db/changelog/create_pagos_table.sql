7dc47b0a6a51af4bd5986cdcc3b8ad2549b882a0
Luciano Alejandro Quequezana Irrarazabal <1633775@utp.edu.pe>
2024-07-22 10:13:38 -0500

    feat: Update 'monto' column data type to INT in 'pagos' table

--- src/main/resources/db/changelog/create_pagos_table.sql
+++ src/main/resources/db/changelog/create_pagos_table.sql
@@ -3,13 +3,13 @@
 
 CREATE TABLE pagos (
     id            BIGINT                                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    monto         DECIMAL(10, 2)                           NOT NULL,
+    monto         INT                          NOT NULL,
     fecha         DATE                                     NOT NULL,
     hora          TIME                                     NULL,
     tipo          enum ('YAPE', 'EFECTIVO', 'TARJETA', 'TRANSFERENCIA') NOT NULL DEFAULT 'EFECTIVO',
     estado        enum ('PENDIENTE', 'APROBADO', 'RECHAZADO') NULL     DEFAULT 'PENDIENTE',
     created_at    TIMESTAMP                                NULL     DEFAULT CURRENT_TIMESTAMP,
     updated_at    TIMESTAMP                                NULL ON UPDATE CURRENT_TIMESTAMP,
-    pedidos_id    BIGINT                                   NOT NULL,
-    FOREIGN KEY (pedidos_id) REFERENCES pedidos (id)
+    reservas_id   BIGINT                                   NOT NULL,
+    FOREIGN KEY (reservas_id) REFERENCES reservas (id)
 );
